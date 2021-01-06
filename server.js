866e4158beb96f8a53a5dfdc739f62be86aa90b2
JeyDev310 <mtroz310@gmail.com>
2021-01-06 10:04:28 +0000

    init project

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+// Allows connection to the CSS file
+app.use(express.static('public'));
+// Accesses bodyParser parser and allows us to access the name of the city the user typed
+app.use(bodyParser.urlencoded({ extended: true }));
+// Sets the template engine
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  console.log('----all')
+  const weather = {description: "desc", temperature: 23.3, windSpeed: 3.5, pressure: 2.5, humidity: 5.3, locationName: "italy"}
+  res.render('index', weather);
+})
+
+app.listen(3000, () => {
+  console.log('Weather app listening on port 3000!')
+})
